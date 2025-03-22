d56da63801fa9770f0f7622a46efb1633259268c
arun <arun.p@tmachinesoftware.com>
2025-03-22 16:53:31 +0530

    Your commit message

--- rest_api_project/api/views.py
+++ rest_api_project/api/views.py
@@ -24,3 +24,4 @@ class ItemViewSet(viewsets.ModelViewSet):
             serializer.save()
             return Response(serializer.data, status=status.HTTP_201_CREATED)
         return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+#this is first push
